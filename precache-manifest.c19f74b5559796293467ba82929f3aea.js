5f18408684d91b5c86d3a61f2c7011bbf02747e4
Ivan Mankovich <mankovich.ivan@gmail.com>
2020-10-06 20:12:33 +0300

    Updates

---
+++ precache-manifest.c19f74b5559796293467ba82929f3aea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "50f5d5418932200fd1e762604a3f2b47",
+    "url": "/mrsoft-test-task/index.html"
+  },
+  {
+    "revision": "71625572403dee044447",
+    "url": "/mrsoft-test-task/static/css/main.98094482.chunk.css"
+  },
+  {
+    "revision": "38f2a042f0f59bc08711",
+    "url": "/mrsoft-test-task/static/js/2.fe6a40ac.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/mrsoft-test-task/static/js/2.fe6a40ac.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71625572403dee044447",
+    "url": "/mrsoft-test-task/static/js/main.b2fa6945.chunk.js"
+  },
+  {
+    "revision": "31ca2d731743891060a6",
+    "url": "/mrsoft-test-task/static/js/runtime-main.d24aa00f.js"
+  }
+]);
